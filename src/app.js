3542500acf45f2b61a1a7ad3b8ef80143b7123b7
yotam.levy <yotam.levy@overwolf.com>
2020-05-15 10:23:02 +0300

    Added more stuff to git ignore

---
+++ src/app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+require('./db/mongoose')
+const userRouter = require('./routers/user')
+const taskRouter = require('./routers/task')
+const maintnanceMode = require('./middleware/maintnance')
+
+const app = express()
+
+app.use(maintnanceMode) //Currently not in maintnance mode
+app.use(express.json())
+app.use(userRouter)
+app.use(taskRouter)
+
+module.exports = app;
